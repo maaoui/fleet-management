cb8c971e6b0f8d6f7f68e7e46ec0844c59f84660
kacperbuczak <kacperbuczak@hotmail.com>
2020-02-27 22:10:28 +0100

    Create BaseEntity.

---
+++ backend/src/main/java/pl/buczak/kacper/fleetmanagement/entity/dao/BaseEntity.java
@@ -0,0 +1,29 @@
+package pl.buczak.kacper.fleetmanagement.entity.dao;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/*
+    @author Kacper Buczak 
+*/
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getCanonicalName() + " ID: " + this.getId();
+    }
+}
