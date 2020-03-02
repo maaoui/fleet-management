029bbedaa76d6542bed2b6e413246752169b7d3c
kacperbuczak <kacperbuczak@hotmail.com>
2020-03-02 20:45:45 +0100

    Refactor employee, add getters,setters.

--- backend/src/main/java/pl/buczak/kacper/fleetmanagement/entity/dao/employee/Employee.java
+++ backend/src/main/java/pl/buczak/kacper/fleetmanagement/entity/dao/employee/Employee.java
@@ -1,9 +1,5 @@
 package pl.buczak.kacper.fleetmanagement.entity.dao.employee;
 
-/*
-    @author Kacper Buczak 
-*/
-
 import pl.buczak.kacper.fleetmanagement.entity.dao.BaseEntity;
 import pl.buczak.kacper.fleetmanagement.entity.dao.department.Department;
 import pl.buczak.kacper.fleetmanagement.entity.dao.vehicle.VehicleUsageTimestamp;
@@ -13,15 +9,12 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.List;
 
+/*
+    @author Kacper Buczak
+*/
 @Entity
 @Table(name = "employee")
 public class Employee extends BaseEntity {
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Department department;
-
-    @OneToMany(cascade = CascadeType.DETACH, mappedBy = "employee")
-    private List<VehicleUsageTimestamp> vehicleUsageTimestamp;
 
     @Size(max = 30)
     @Column(nullable = false, name = "first_name")
@@ -40,5 +33,60 @@ public class Employee extends BaseEntity {
     @Pattern(regexp = "[1-9][0-9]{8}")
     private String phoneNumber;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Department department;
 
+    @OneToMany(cascade = CascadeType.DETACH, mappedBy = "employee")
+    private List<VehicleUsageTimestamp> vehicleUsageTimestamp;
+
+    public Employee() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<VehicleUsageTimestamp> getVehicleUsageTimestamp() {
+        return vehicleUsageTimestamp;
+    }
+
+    public void setVehicleUsageTimestamp(List<VehicleUsageTimestamp> vehicleUsageTimestamp) {
+        this.vehicleUsageTimestamp = vehicleUsageTimestamp;
+    }
 }
