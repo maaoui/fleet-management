5b18bef73914490c70353458de8e37d36f432496
kacperbuczak <kacperbuczak@hotmail.com>
2020-03-14 20:29:53 +0100

    Create repositories for role, employee.

--- backend/src/main/java/pl/buczak/kacper/fleetmanagement/repository/employee/EmployeeRepository.java
+++ backend/src/main/java/pl/buczak/kacper/fleetmanagement/repository/employee/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package pl.buczak.kacper.fleetmanagement.repository.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.buczak.kacper.fleetmanagement.entity.dao.employee.Employee;
 
@@ -9,4 +11,7 @@ import pl.buczak.kacper.fleetmanagement.entity.dao.employee.Employee;
 */
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(value = "SELECT e FROM Employee e WHERE e.email = :email")
+    Employee findbyEmail(@Param("email") String email);
 }
---
+++ backend/src/main/java/pl/buczak/kacper/fleetmanagement/repository/employee/RoleRepository.java
@@ -0,0 +1,15 @@
+package pl.buczak.kacper.fleetmanagement.repository.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pl.buczak.kacper.fleetmanagement.entity.dao.employee.Role;
+
+/*
+    @author Kacper Buczak 
+*/
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    @Query(value = "SELECT r FROM Role r where r.name = :roleName")
+    Role findByName(@Param("roleName") String roleName);
+}
