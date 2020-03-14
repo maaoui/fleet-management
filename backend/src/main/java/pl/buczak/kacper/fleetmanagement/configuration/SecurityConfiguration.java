675bd14a01cd235a8433ddd22d4e3c31c9af9e21
kacperbuczak <kacperbuczak@hotmail.com>
2020-03-14 20:28:04 +0100

    Create security config.

---
+++ backend/src/main/java/pl/buczak/kacper/fleetmanagement/configuration/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package pl.buczak.kacper.fleetmanagement.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/*
+    @author Kacper Buczak 
+*/
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("*/**")
+                .hasRole("EMPLOYEE");
+    }
+}
