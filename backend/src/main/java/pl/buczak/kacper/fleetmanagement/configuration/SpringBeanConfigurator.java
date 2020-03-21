26ec38f76d1132e1d0f4c3c267ca3e3a56428855
kacperbuczak <kacperbuczak@hotmail.com>
2020-03-21 15:02:39 +0100

    Create bean configuration class, add model mapper

---
+++ backend/src/main/java/pl/buczak/kacper/fleetmanagement/configuration/SpringBeanConfigurator.java
@@ -0,0 +1,20 @@
+package pl.buczak.kacper.fleetmanagement.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+    @author Kacper Buczak 
+*/
+@Configuration
+public class SpringBeanConfigurator {
+
+    public SpringBeanConfigurator() {
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
