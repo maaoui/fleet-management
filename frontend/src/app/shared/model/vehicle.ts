a8513c0d4a3be299c426c4d7362571146ead0428
kacperbuczak <kacperbuczak@hotmail.com>
2020-03-29 19:17:07 +0200

    Create vehicle class.

---
+++ frontend/src/app/shared/model/vehicle.ts
@@ -0,0 +1,27 @@
+export interface VehicleAttrs {
+  id: number;
+  plateNumber: string;
+  make: string;
+  model: string;
+  horsePower: number;
+  vin: string;
+
+}
+
+export class Vehicle {
+  constructor(attrs: Partial<VehicleAttrs> = {}) {
+    this.id = attrs.id;
+    this.plateNumber = attrs.plateNumber;
+    this.make = attrs.make;
+    this.model = attrs.model;
+    this.horsePower = attrs.horsePower;
+    this.vin = attrs.vin;
+  }
+
+  id: number;
+  plateNumber: string;
+  make: string;
+  model: string;
+  horsePower: number;
+  vin: string;
+}
