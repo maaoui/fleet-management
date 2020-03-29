081faaff3d83eae6d2baf5133a52fdae818c3744
kacperbuczak <kacperbuczak@hotmail.com>
2020-03-29 16:49:28 +0200

    Implement loading admin components.

--- frontend/src/app/admin/admin-view/admin-view.component.spec.ts
+++ frontend/src/app/admin/admin-view/admin-view.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AdminViewComponent } from './admin-view.component';
+import {AdminViewComponent} from './admin-view.component';
 
 describe('AdminViewComponent', () => {
   let component: AdminViewComponent;
@@ -8,9 +8,9 @@ describe('AdminViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminViewComponent ]
+      declarations: [AdminViewComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- frontend/src/app/admin/admin-view/admin-view.component.ts
+++ frontend/src/app/admin/admin-view/admin-view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ComponentFactory, ComponentFactoryResolver, ComponentRef, OnInit, ViewContainerRef} from '@angular/core';
+import {AdminVehiclesComponent} from '../admin-vehicles/admin-vehicles.component';
+import {AdminExpensesComponent} from '../admin-expenses/admin-expenses.component';
+import {AdminRepairsComponent} from '../admin-repairs/admin-repairs.component';
 
 @Component({
   selector: 'app-admin-view',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-view.component.scss']
 })
 export class AdminViewComponent implements OnInit {
+  private currentComponent: ComponentRef<any> = null;
 
-  constructor() { }
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,
+              private viewContainerRef: ViewContainerRef) {
+  }
 
   ngOnInit(): void {
   }
 
+  createAdminVehiclesComponent(): void {
+    this.destroyCurrentComponent();
+    this.createComponent(this.getAdminVehiclesComponentFactory());
+  }
+
+  createAdminRepairsComponent() {
+    this.destroyCurrentComponent();
+    this.createComponent(this.getAdminRepairsComponentFactory());
+  }
+
+  createAdminExpensesComponent() {
+    this.destroyCurrentComponent();
+    this.createComponent(this.getAdminExpensesComponentFactory());
+  }
+
+  createComponent(componentRef: ComponentFactory<any>): void {
+    this.currentComponent = this.viewContainerRef.createComponent(componentRef);
+    this.currentComponent.changeDetectorRef.detectChanges();
+  }
+
+  destroyCurrentComponent(): void {
+    if (this.currentComponent) {
+      this.currentComponent.destroy();
+    }
+  }
+
+  private getAdminVehiclesComponentFactory(): ComponentFactory<AdminVehiclesComponent> {
+    return this.componentFactoryResolver.resolveComponentFactory(AdminVehiclesComponent);
+  }
+
+  private getAdminRepairsComponentFactory(): ComponentFactory<AdminRepairsComponent> {
+    return this.componentFactoryResolver.resolveComponentFactory(AdminRepairsComponent);
+  }
+
+  private getAdminExpensesComponentFactory(): ComponentFactory<AdminExpensesComponent> {
+    return this.componentFactoryResolver.resolveComponentFactory(AdminExpensesComponent);
+  }
 }
