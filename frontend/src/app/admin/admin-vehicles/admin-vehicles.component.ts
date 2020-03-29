bb3f2428c5d6b11961c7297b70d94e570acf4615
kacperbuczak <kacperbuczak@hotmail.com>
2020-03-29 19:18:58 +0200

    Implement simple list.

--- frontend/src/app/admin/admin-vehicles/admin-vehicles.component.ts
+++ frontend/src/app/admin/admin-vehicles/admin-vehicles.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Vehicle} from '../../shared/model/vehicle';
+import {VehicleService} from '../../shared/service/vehicle.service';
 
 @Component({
   selector: 'app-admin-vehicles',
@@ -6,11 +8,27 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./admin-vehicles.component.scss']
 })
 export class AdminVehiclesComponent implements OnInit {
+  private vehicles: Vehicle[];
 
-  constructor() {
+  constructor(private vehicleService: VehicleService) {
   }
 
   ngOnInit(): void {
+    this.loadVehiclesList();
   }
 
+  private loadVehiclesList() {
+    this
+      .vehicleService
+      .getVehiclesList()
+      .subscribe(
+        (vehicles) => {
+          this.vehicles = vehicles;
+          console.log(vehicles);
+        },
+        (error) => {
+          // TODO Handle error - show error message
+        }
+      );
+  }
 }
